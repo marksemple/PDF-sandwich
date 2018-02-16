085d6e80c706a218865d94ac276ed0f6f1a8b912
Mark Semple <mark.joseph.semple@gmail.com>
2018-02-16 13:31:30 -0500

    added some documentation and a gitignore.

--- pdf_manipulation.py
+++ pdf_manipulation.py
@@ -1,49 +1,56 @@
-# pdf_manipulation.py
-
+#! python3
 import PyPDF2
 
-def interleavePDFs(coverPDFpath, internalPDFpath, outputPDFpath):
-    """ Puts all of internalPDF inbetween first and last pages of coverPDF
-        Saves new PDF at outputPath """
 
-    pdfwriter = PyPDF2.PdfFileWriter()
+def interleavePDFs(inner_pdf_path, outer_pdf_path, destination_pdf_path):
+    """ Embeds 'inner_PDF' between pages of 'outer_PDF'
+        Saves a new PDF at 'destination'.
 
-    # get FRONTPAGE and BACKPAGE from the cover pdf
-    coverFile = open(coverPDFpath, 'rb')
-    innerFile = open(internalPDFpath, 'rb')
+        Created mostly because I don't have a printer at home,
+        and I don't want to get in trouble for using shared work printer
+        for printing personal items (concert tickets,.. etc)
+    """
 
+    # get FRONTPAGE and BACKPAGE from the OUTER-PDF
+    coverFile = open(outer_pdf_path, 'rb')
     coverreader = PyPDF2.PdfFileReader(coverFile)
     FrontPage = coverreader.getPage(0)
     BackPage = coverreader.getPage(coverreader.numPages - 1)
 
+    # write Cover Page
+    pdfwriter = PyPDF2.PdfFileWriter()
     pdfwriter.addPage(FrontPage)
 
+    # get pages from INNER-PDF
+    innerFile = open(inner_pdf_path, 'rb')
     innerreader = PyPDF2.PdfFileReader(innerFile)
     for pageIdx in range(innerreader.numPages):
         pageObj = innerreader.getPage(pageIdx)
         pdfwriter.addPage(pageObj)
 
+    # write back page
     pdfwriter.addPage(BackPage)
 
-    outputFile = open(outputPDFpath, 'wb')
+    # Save sandwiched PDF to file
+    outputFile = open(destination_pdf_path, 'wb')
     pdfwriter.write(outputFile)
 
-    coverFile.close()
     innerFile.close()
+    coverFile.close()
     outputFile.close()
 
 
 if __name__ == "__main__":
+    # RUN ME for a demo.
 
     import os
 
-    head, tail = os.path.split(__file__)
+    thisFolder, thisFile = os.path.split(__file__)
 
+    outerFile = os.path.join(thisFolder, 'demo_outer_pages.pdf')
+    innerFile = os.path.join(thisFolder, 'demo_inner_pages.pdf')
+    destination = os.path.join(thisFolder, 'demo_combined_output.pdf')
 
-    coverFile = os.path.join(head, 'external_pages.pdf')
-    innerFile = os.path.join(head, 'internal_pages.pdf')
-    destination = os.path.join(head, 'combinedPDF.pdf')
-
-    interleavePDFs(coverFile, innerFile, destination)
-
-
+    interleavePDFs(inner_pdf_path=innerFile,
+                   outer_pdf_path=outerFile,
+                   destination_pdf_path=destination)
